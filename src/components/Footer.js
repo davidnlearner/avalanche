ec5e6d207bdbb5c5f0b8dac397779e94d5ebeda2
David Learner <david.learner@gmail.com>
2021-09-10 13:01:38 -0500

    fixing links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,18 @@ export default function Footer() {
     return (
         <div className="footer">
             Made by{" "}
-            <a href="https://www.david-learner.com/" target="_blank">
+            <a
+                href="https://www.david-learner.com/"
+                target="_blank"
+                rel="noreferrer"
+            >
                 David Learner
             </a>
             . Code for this project lives{" "}
             <a
                 href="https://github.com/davidnlearner/avalanche"
                 target="_blank"
+                rel="noreferrer"
             >
                 here
             </a>
