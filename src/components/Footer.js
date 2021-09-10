3d8b034e60cb460de2e4b08615465452aafa65fa
David Learner <david.learner@gmail.com>
2021-09-10 12:54:01 -0500

    meta tags added, linksnow open on new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 export default function Footer() {
     return (
         <div className="footer">
-            Made by <a href="https://www.david-learner.com/">David Learner</a>.
-            Code for this project lives{" "}
-            <a href="https://github.com/davidnlearner/avalanche">here</a>.
+            Made by{" "}
+            <a href="https://www.david-learner.com/" target="_blank">
+                David Learner
+            </a>
+            . Code for this project lives{" "}
+            <a
+                href="https://github.com/davidnlearner/avalanche"
+                target="_blank"
+            >
+                here
+            </a>
+            .
         </div>
     );
 }
